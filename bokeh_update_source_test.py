d6297db10d5e5e49e39e83d0b93f454af2a03853
npricejones <price-jones@astro.utoronto.ca>
2018-07-17 11:42:35 -0400

    Checking update behaviour when combining python and JS callbacks

--- bokeh_update_source_test.py
+++ bokeh_update_source_test.py
@@ -10,12 +10,9 @@ from random import random
 key_list = list('ABCDEFGHIJKLMNOP')
 
 DATA1 = {key:[random() for i in range(10)] for key in key_list}
-DATA2 = {key:[random() for i in range(15)] for key in key_list}
 DATA1['xaxis'] = range(10)
-DATA2['xaxis'] = range(15)
 
 source1 = ColumnDataSource(data=DATA1)
-source2 = ColumnDataSource(data=DATA2)
 
 fill_source = ColumnDataSource(data=DATA1)
 
@@ -24,32 +21,30 @@ fig = figure()
 for key in key_list:
     fig.circle(x='xaxis',y=key,source=fill_source)
 
-select = Select(options=['source1','source2'],value='source1')
+def newsource2(attrs,old,new):
+	DATA2 = {key:[random() for i in range(15)] for key in key_list}
+	DATA2['xaxis'] = range(15)
+	source2 = ColumnDataSource(data=DATA2)
+	select.callback = CustomJS(args=dict(source=fill_source,source1=source1,source2=source2),code=codes)
 
+select = Select(options=['source1','source2'],value='source1')
 codes = """
 var f = cb_obj.value;
 var sdata = source.data;
 var data1 = source1.data;
 var data2 = source2.data;
 
-if (f == "source1") {
-for (key in data1) {
-    sdata[key] = [];
-    for (i=0;i<data1[key].length;i++){
-    sdata[key].push(data1[key][i]);
-    }
-}
-} else {
 for (key in data2) {
     sdata[key] = [];
     for (i=0;i<data2[key].length;i++){
     sdata[key].push(data2[key][i]);
     }
 }
-};
+
 console.log("Hello world");
 source.change.emit();
 """
-select.callback = CustomJS(args=dict(source=fill_source,source1=source1,source2=source2),code=codes)
+select.on_change('value',newsource2)
+select.callback = CustomJS(args=dict(source=fill_source,source1=source1,source2=source1),code=codes)
 
 curdoc().add_root(column(widgetbox(select), fig, width=1100))
