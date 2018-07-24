be927b217049ead1c0b9f386fc859a4fa526fe41
NatalieP-J <natalie.price.jones@mail.utoronto.ca>
2018-07-24 19:38:37 -0400

    different timestemp choice

--- selection_histogram.py
+++ selection_histogram.py
@@ -946,5 +946,5 @@ for (key in vnew{0}) {{
             c4.glyph.y = 'avgfsi'
 
 
-starter = display_result(case='8',timestamp='2018-07-24.19.34.07.531753',pad=0.1)
+starter = display_result(case='8',timestamp='2018-07-24.19.46.04.809278',pad=0.1)
 
