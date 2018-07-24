418aa7b6cf444e5dd4176b567be19552acf25652
NatalieP-J <natalie.price.jones@mail.utoronto.ca>
2018-07-24 17:54:00 -0400

    Updating case files

--- case8.py
+++ case8.py
@@ -15,26 +15,31 @@ crossfitatms = [6,7,8,11,12,13,14,16,19,20,22,23,25,26,28] # atomic numbers of c
 spreadchoice = spreads # choose which abudance spreads to employ
 
 # DBSCAN parameters                                                             
-smin_samples = np.array([2,3])
-ssamples = len(smin_samples)
+min_samples = np.array([2,3])
+samples = len(min_samples)
 lg = np.arange(-3,1)
 eps = [i*10.**lg for i in [1,5]]
 eps = np.concatenate((eps[0],eps[1]))
 eps.sort()
-seps = eps
-smin_samples = np.tile(smin_samples,len(seps))
-amin_samples = np.array([2,3])
-asamples = len(amin_samples)
-aeps = eps
-amin_samples = np.tile(amin_samples,len(aeps))
-aeps = np.repeat(aeps,asamples)
-seps = np.repeat(seps,ssamples)
+eps = np.array([0.07,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0])
+min_samples = np.tile(min_samples,len(eps))
+eps = np.repeat(eps,samples)
 
 case8 = caserun(nstars=nstars,sample=sample,abundancefac=abundancefac,
-                 spreadchoice=spreadchoice,specfac=specfac,
+                 spreadchoice=spreadchoice,specfac=specfac,centerfac=centerfac,
+                 centerspr=spreads,genfn=choosestruct,
                  fullfitkeys=fullfitkeys,fullfitatms=fullfitatms,
                  crossfitkeys=crossfitkeys,crossfitatms=crossfitatms,
-                 seps=seps,smin_samples=smin_samples,
-                 aeps=aeps,amin_samples=amin_samples,
-                 metric='precomputed',neighbours = 20,phvary=True,
-                 fitspec=True,case='8',centerfac=centerfac,normeps=True)
+                 phvary=True,fitspec=True,case='8')
+start = time.time()
+case8.clustering(case8.specinfo.spectra,'spec',eps,min_samples,metric='precomputed',
+                neighbours = 20,normeps=normeps)
+case8.clustering(case8.abundances,'abun',eps,min_samples,metric='precomputed',
+                neighbours = 20,normeps=normeps)
+toph = combine_windows(windows = tophats,combelem=elem)
+case8.projspec(toph)
+case8.clustering(case8.projectspec,'toph',eps,min_samples,metric='precomputed',
+                neighbours = 20,normeps=normeps)
+end = time.time()
+case8.finish()
+print('Finished desired clustering in {0} seconds'.format(end-start))
