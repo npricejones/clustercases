8409e1300393e02333a69f402f4ac7fbef2ddda5
npricejones <price-jones@astro.utoronto.ca>
2018-08-02 14:51:41 -0400

    Made a quick script to run all relevant cases in series

---
+++ seriesrun.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source ~/.bash_profile
+
+py3
+
+python3 case1.py
+python3 case2.py
+python3 case4.py
+python3 case6.py
+python3 case8.py
+python3 case12.py
+python3 case14.py
+python3 case16.py
+python3 case20.py
+python3 case22.py
+python3 case24.py
+python3 case28.py
+python3 case30.py
+python3 case32.py
+
