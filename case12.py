465c48bdaffe8a98b7ac9966703ac61833b230bf
NatalieP-J <natalie.price.jones@mail.utoronto.ca>
2018-09-11 15:15:52 -0400

    Adding version to run on red clump/red giants

--- case12.py
+++ case12.py
@@ -1,8 +1,8 @@
 from case_template import *
 
 # run parameters                                                               
-nstars = 1e3 # number of stars                                                 
-sample='allStar_chemscrub_teffcut_dr14.npy' # APOGEE sample to draw from            
+nstars = 1e4 # number of stars                                                 
+sample='red_giant_teffcut_dr14.npy' # APOGEE sample to draw from            
 abundancefac = 1 # scaling factor for abundance noise                          
 specfac = 1e-2 # scaling factor for spectra noise                              
 centerfac = 1
@@ -16,7 +16,7 @@ spreadchoice = spreads # choose which abudance spreads to employ
 
 # DBSCAN parameters                                                             
 
-min_samples = np.array([2,3])#,4,5,6,7,8,9,10,11,12,13,14,15,18,20,30,40,50])
+min_samples = np.array([2,3,4,5,6,7,8,9,10,11,12,13,14,15,18,20,30,40,50])
 samples = len(min_samples)
 eps = np.array([0.01,0.02,0.04,0.05,0.06,0.07,0.08,0.09,0.1,0.12,0.15,0.19,0.24,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0])
 min_samples = np.tile(min_samples,len(eps))
